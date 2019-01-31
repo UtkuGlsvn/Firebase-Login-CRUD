98f16bddd5bd9fd6b44b07a4d7edd12779ddf1c9
utkuglsvn <utku.glsvn@gmail.com>
2019-01-31 21:38:56 +0300

    Login java complete

--- app/src/main/java/com/example/myfirebase/Login.java
+++ app/src/main/java/com/example/myfirebase/Login.java
@@ -1,25 +1,44 @@
 package com.example.myfirebase;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     TextView signup;
     Button login;
     EditText email,password;
+    private FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         signup=findViewById(R.id.signup);
         login=findViewById(R.id.btn_login);
         email=findViewById(R.id.login_mail);
         password=findViewById(R.id.login_password);
+
+        auth = FirebaseAuth.getInstance();
+/*
+        if (auth.getCurrentUser() != null) {
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            finish();
+        }*/
+        auth = FirebaseAuth.getInstance();
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,14 +51,48 @@ public class Login extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String mail=email.getText().toString();
+                String pswd = password.getText().toString();
+                loginControl(mail,pswd);
             }
         });
     }
 
-    boolean emptyCheck(String mail, String password)
+    boolean emptyCheck(String email, String password)
     {
-        if(mail.isEmpty() && password.isEmpty()) return  true;
-        else  return  false;
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+       return  true;
+    }
+    void loginControl(String email, final String password)
+    {
+        if(emptyCheck( email,password)) {
+            auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                // there was an error
+                                if (password.length() < 6) {
+                                    Toast.makeText(getBaseContext(), "The password should be 6 characters at least!", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(getBaseContext(), "no membership found...", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Intent intent = new Intent(Login.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                                Toast.makeText(getBaseContext(), "Successful Login", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
     }
 }
