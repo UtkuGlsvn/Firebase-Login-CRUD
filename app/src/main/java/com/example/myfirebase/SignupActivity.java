946d8f93734a51c6df7e84cd5fd709e4ad32ca3f
utkuglsvn <utku.glsvn@gmail.com>
2019-01-31 22:22:40 +0300

    Signup java complete

--- app/src/main/java/com/example/myfirebase/SignupActivity.java
+++ app/src/main/java/com/example/myfirebase/SignupActivity.java
@@ -1,9 +1,11 @@
 package com.example.myfirebase;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,6 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -28,7 +31,6 @@ public class SignupActivity extends AppCompatActivity {
         create=findViewById(R.id.btn_create);
         email=findViewById(R.id.email);
         password=findViewById(R.id.password);
-
         myAut=FirebaseAuth.getInstance();
         dialog=new ProgressDialog(this);
 
@@ -41,25 +43,44 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
-    boolean emptyCheck(String mail, String password)
+    boolean emptyCheck(String email, String password)
     {
-        if(mail.isEmpty() && password.isEmpty()) return  true;
-        else  return  false;
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return  true;
     }
     void addAccount(String mail, String password)
     {
-        if(emptyCheck(mail,password)==true)
+        if(emptyCheck(mail,password))
         {
+            dialog.setMessage("loading...");
+            dialog.show();
             myAut.createUserWithEmailAndPassword(mail,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if(task.isSuccessful())
-                    Toast.makeText(getBaseContext(), "Successful", Toast.LENGTH_SHORT).show();
+                    if(task.isSuccessful()) {
+                        Toast.makeText(getBaseContext(), "Successful!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(SignupActivity.this, MainActivity.class));
+                    }
+                    else
+                        Toast.makeText(getBaseContext(), "Problem...", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
                 }
             });
 
         }
-        else             Toast.makeText(getBaseContext(), "required field", Toast.LENGTH_SHORT).show();
+
 
     }
 
