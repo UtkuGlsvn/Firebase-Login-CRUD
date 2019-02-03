f26843b61bf26f526803e007dd023e622435e482
utkuglsvn <utku.glsvn@gmail.com>
2019-02-03 22:49:00 +0300

    Model

---
+++ app/src/main/java/com/example/myfirebase/Model.java
@@ -0,0 +1,58 @@
+package com.example.myfirebase;
+
+public class Model {
+
+
+    private String data,name,surname,gender,id;
+
+    public Model(String id,String data, String name, String surname, String gender) {
+        this.id=id;
+        this.data = data;
+        this.name = name;
+        this.surname = surname;
+        this.gender = gender;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
